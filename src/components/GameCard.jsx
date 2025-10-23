a7384f0fca19e63a120c582ecaedb84b0674e03f
Fábio Santos <fabiospinheira@gmail.com>
2025-10-23 14:54:27 -0300

    feat(main-screen): cards hover effect

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,15 +1,32 @@
 import "./GameCard.css";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function GameCard({ game }) {
   const navigate = useNavigate();
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleClick = () => {
     navigate(`/game/${encodeURIComponent(game.name)}`);
   };
 
   return (
-    <div className="game-card cursor-target" onClick={handleClick}>
+    <div
+      className="game-card cursor-target"
+      onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      {isHovered && (
+        <div>
+          <img className="game-background" src={game.image} alt={game.name} />
+          <img
+            className="game-background blur"
+            src={game.image}
+            alt={game.name}
+          />
+        </div>
+      )}
       <img className="game-card__image" src={game.image} alt={game.name} />
       <h2 className="game-card__title">{game.name}</h2>
       <p className="game-card__description">
